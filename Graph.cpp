c832849facb38720cbf54299204675f9dda41bb7
Tilman <twittl@gmx.de>
2012-06-24 16:38:15 +0200

    minimal class for a directed graph

---
+++ Graph.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Graph.cpp
+ * Author: tilman
+ * 
+ * Created on June 24, 2012, 4:36 PM
+ */
+
+#include "Graph.h"
+
+Graph::Graph() {
+}
+
+Graph::Graph(const Graph& orig) {
+}
+
+Graph::~Graph() {
+}
+
---
+++ Graph.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Graph.h
+ * Author: tilman
+ *
+ * Created on June 24, 2012, 4:36 PM
+ */
+
+#ifndef GRAPH_H
+#define	GRAPH_H
+
+class Graph {
+public:
+    Graph();
+    Graph(const Graph& orig);
+    virtual ~Graph();
+private:
+
+};
+
+#endif	/* GRAPH_H */
+
