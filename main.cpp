7ced54324be9834e86a6fb6566bfeb69be849a96
Tilman <twittl@gmx.de>
2012-06-24 16:32:14 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: tilman
+ *
+ * Created on June 24, 2012, 4:24 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
