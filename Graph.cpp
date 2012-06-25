8c1136b19663c31674dd44bea4921605678f3402
Tilman <twittl@gmx.de>
2012-06-25 21:16:36 +0200

    fixed some issues

--- Graph.cpp
+++ Graph.cpp
@@ -8,23 +8,36 @@
 #include "Graph.h"
 #include <vector>
 
-
+/*
+ * Note: We assume, that the nodes are named by numbers in the range between 1 
+ * and |V|. The range of the indices of the adjacency matrix is between 0 and 
+ * |V|-1, therefore we substracted 1 from the numbers we get.
+ */
 Graph::Graph(int V) {
     numberOfNodes = V;
     //create the empty adjacency matrix
-    std::vector<std::vector<int> > adj(numberOfNodes, std::vector<int>(numberOfNodes,0));
+    (*this).adj.resize(numberOfNodes, std::vector<int>(numberOfNodes, 0));
     //create the empty capacity matrix
-    std::vector<std::vector<int> > cap(numberOfNodes, std::vector<int>(numberOfNodes,0));
+    (*this).cap.resize(numberOfNodes, std::vector<int>(numberOfNodes, 0));
+    
 }
 
 
-
+/* Method for adding edges to the adjacency matrix.
+ */
 void Graph::addEdge(int u, int v, int cuv, int cvu) {
-    adj.at(u-1).at(v-1) = 1;
-    cap.at(u-1).at(v-1) = cuv;
+    (*this).adj[u-1][v-1] = 1;
+    (*this).cap[u-1][v-1] = cuv;
     if(cvu>0) {
-        adj.at(v-1).at(u-1) = 1;
-        cap.at(v-1).at(u-1) = cvu;
+        (*this).adj[v-1][u-1] = 1;
+        (*this).cap[v-1][u-1] = cvu;
     }
 }
 
+
+
+/* overwritten << operator
+ */
+std::ostream& operator<<(std::ostream &strm, const Graph &g) {
+    
+}
--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 /* 
  * File:   main.cpp
- * Author: tilman
+ * Author: Tilman
  *
  * Created on June 24, 2012, 4:24 PM
  */
 
-#include <cstdlib>
 
+#include <iostream> 
+#include <unistd.h>
+#include <fstream>
+#include "Graph.h"
 using namespace std;
 
-/*
- * 
- */
+void printHelp() {
+    std::cout << "Usage: $ ./preflowPush -f [graph] -a [random|FIFO|Highest-Label|Excess-Scaling]"<< std::endl;   
+}
+
+
 int main(int argc, char** argv) {
 
+    
+    
+    Graph g(4);
+    g.addEdge(1,2,5,0);
+    g.addEdge(1,3,2,0);
+    g.addEdge(2,3,5,3);
+    g.addEdge(3,4,7,0);
+    cout << g;
     return 0;
 }
 
