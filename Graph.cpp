31dbac8672570fc2b911cf5aa4d273baab0b2571
Tilman <twittl@gmx.de>
2012-06-25 17:18:05 +0200

    Example Graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,18 +1,30 @@
 /* 
  * File:   Graph.cpp
- * Author: tilman
+ * Author: Tilman
  * 
  * Created on June 24, 2012, 4:36 PM
  */
 
 #include "Graph.h"
+#include <vector>
 
-Graph::Graph() {
+
+Graph::Graph(int V) {
+    numberOfNodes = V;
+    //create the empty adjacency matrix
+    std::vector<std::vector<int> > adj(numberOfNodes, std::vector<int>(numberOfNodes,0));
+    //create the empty capacity matrix
+    std::vector<std::vector<int> > cap(numberOfNodes, std::vector<int>(numberOfNodes,0));
 }
 
-Graph::Graph(const Graph& orig) {
-}
 
-Graph::~Graph() {
+
+void Graph::addEdge(int u, int v, int cuv, int cvu) {
+    adj.at(u-1).at(v-1) = 1;
+    cap.at(u-1).at(v-1) = cuv;
+    if(cvu>0) {
+        adj.at(v-1).at(u-1) = 1;
+        cap.at(v-1).at(u-1) = cvu;
+    }
 }
 
