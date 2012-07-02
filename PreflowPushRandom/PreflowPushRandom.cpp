9f14158fb3ef2d90236a9604755fcc786b00ea06
haas <haas@pool18>
2012-07-02 16:58:44 +0200

    Add stubs for Random Preflow algorithm

---
+++ PreflowPushRandom/PreflowPushRandom.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   PreflowPushRandom.cpp
+ * Author: haas
+ * 
+ * Created on 2. Juli 2012, 16:55
+ */
+
+#include "PreflowPushRandom.h"
+
+PreflowPushRandom::PreflowPushRandom() {
+}
+
+PreflowPushRandom::PreflowPushRandom(const PreflowPushRandom& orig) {
+}
+
+PreflowPushRandom::~PreflowPushRandom() {
+}
+
---
+++ PreflowPushRandom/PreflowPushRandom.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   PreflowPushRandom.h
+ * Author: haas
+ *
+ * Created on 2. Juli 2012, 16:55
+ */
+
+#ifndef PREFLOWPUSHRANDOM_H
+#define	PREFLOWPUSHRANDOM_H
+
+class PreflowPushRandom : public PreflowPushGeneric {
+public:
+    PreflowPushRandom();
+    PreflowPushRandom(const PreflowPushRandom& orig);
+    virtual ~PreflowPushRandom();
+private:
+
+};
+
+#endif	/* PREFLOWPUSHRANDOM_H */
+
