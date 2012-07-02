e73ce18f1556e0268c89a3d48e0be9c161015d8d
haas <haas@pool18>
2012-07-02 16:09:41 +0200

    CLI arg parsing and cow powers

--- main.cpp
+++ main.cpp
@@ -12,14 +12,57 @@
 #include "Graph.h"
 #include <assert.h>
 #include "ProblemTest.h"
+#include <fstream>
 using namespace std;
 
 void printHelp() {
-    std::cout << "Usage: $ ./preflowPush -f [graph] -a [random|FIFO|Highest-Label|Excess-Scaling]" << std::endl;
+    std::cout << "Usage: $ ./preflowPush -f [graph] -a [Random|FIFO|Highest-Label|Excess-Scaling|Run-Tests]" << std::endl;
 }
 
 int main(int argc, char** argv) {
-
+        
+    if (argc < 5 || string(argv[1]) != string("-f") || string(argv[3]) != string("-a"))
+    {
+        printHelp();
+        exit(1);
+    }
+        
+    string graphFile = argv[2];
+    ifstream in(graphFile.c_str());
+    Graph inputGraph(in);
+    string algType = argv[4];
+    
+    if (algType == "Random"){
+        cerr << "Not implemented." << endl;
+    }
+    else if (algType == "FIFO")
+    {
+        cerr << "Not implemented." << endl;
+    }
+    else if (algType == "Highest-Label"){
+        cerr << "Not implemented." << endl;
+    }
+    else if (algType == "Excess-Scaling"){
+        cerr << "Not implemented." << endl;
+    }
+    else if (algType == "Run-Tests")
+    {
+        cout << "Running tests..." << endl;
+        ProblemTest tester;
+        tester.run();
+    } else if (algType == "Moo") {
+        cout << "        (__)" << endl;
+        cout << "        (oo)" << endl;
+        cout << "  /------\\/" << endl;
+        cout << " / |    ||" << endl;
+        cout << "*  /\\---/\\" << endl;
+        cout << "   ~~   ~~ " << endl;
+    } else
+    {
+        std::cout << "Unknown algorithm type." << std::endl;
+    }
+   
+/*
     Graph g(4);
     g.addEdge(1, 2, 5, 0);
     g.addEdge(1, 3, 2, 0);
@@ -36,11 +79,9 @@ int main(int argc, char** argv) {
    
     cout << "Graph from disk:" << endl;
     cout << readGraph << endl;
-
+   */
     
-    cout << "Running tests..." << endl;
-    ProblemTest tester;
-    tester.run();
+
     return 0;
     
     
