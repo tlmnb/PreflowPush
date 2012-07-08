ad37de4cb9605a1d0447cf765b14094eb82dbac3
Tilman <twittl@gmx.de>
2012-07-08 14:04:47 +0200

    started to implement PreflowPushHighestLabel

--- PreflowPushHighestLabel/PreflowPushHighestLabel.cpp
+++ PreflowPushHighestLabel/PreflowPushHighestLabel.cpp
@@ -6,13 +6,35 @@
  */
 
 #include "PreflowPushHighestLabel.h"
+#include <algorithm>
 
-PreflowPushHighestLabel::PreflowPushHighestLabel() {
+PreflowPushHighestLabel::PreflowPushHighestLabel(Graph* g) :  PreflowPushGeneric(g) {
 }
 
-PreflowPushHighestLabel::PreflowPushHighestLabel(const PreflowPushHighestLabel& orig) {
+PreflowPushHighestLabel::PreflowPushHighestLabel(const PreflowPushHighestLabel& orig) : PreflowPushGeneric(orig) {
 }
 
 PreflowPushHighestLabel::~PreflowPushHighestLabel() {
 }
+
+PreflowPushHighestLabel::Comperator::Comperator(PreflowPushHighestLabel* phl) {
+    (*this).phl = phl;
+}
+
+bool PreflowPushHighestLabel::Comperator::operator ()(const int& a, const int& b) {
+    return (*this).phl->getH(a) < (*this).phl->getH(b);
+}
+
+
+void PreflowPushHighestLabel::exec() {
+    std::vector<int> nodes; //all nodes
+    for(int i=0; i<(*this).g->getNumberOfNodes(); i++) {
+        nodes.push_back(i);
+    }
+    
+    Comperator comp(this);
+    std::make_heap(nodes.begin(),nodes.end(),comp);
+    
+}
+
 
